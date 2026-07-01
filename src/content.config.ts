7fbda1adc3fdd87683d1f2220ebc6f3bcee86aa5
r1pple <1303438967@qq.com>
2026-07-01 21:14:02 +0800

    feat: add sample content and favicon, fix Astro 7 content layer compat

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,10 +1,14 @@
 import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
 
-const blog = defineCollection({
+const blogCollection = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/content/blog" }),
   schema: z.object({
     title: z.string(),
     date: z.date(),
   }),
 });
 
-export const collections = { blog };
+export const collections = {
+  blog: blogCollection,
+};
