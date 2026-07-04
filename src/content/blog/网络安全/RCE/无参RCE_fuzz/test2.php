9beeb074efbc2ecb7f94108ea737e65255a6d9f6
r1pple <1303438967@qq.com>
2026-07-04 11:17:30 +0800

    Switch content blog to git submodule (note-remote)

--- src/content/blog/网络安全/RCE/无参RCE_fuzz/test2.php
+++
@@ -1,7 +0,0 @@
-<?php
-$test1 =  gzcompress("abcdef");
-echo $test1 .PHP_EOL;
-
-$test2 =  gzuncompress($test1);
-echo $test2;
-?>
--- src/content/blog/网络安全/php伪协议/靶场/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-highlight_file(__FILE__);
-if (isset($_GET['page'])) {
-    include $_GET['page'];
-} else {
-    echo "Welcome to the PHP pseudo-protocol lab!";
-}
-?>
